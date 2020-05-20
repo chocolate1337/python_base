5f7b05897913c70eac2bc285bd7ae013579b06a5
Vlad <kill.lilo2@yandex.ru>
2020-05-20 21:07:20 +0300

    04 выполнил

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -11,4 +11,13 @@
 
 educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+money = educational_grant - expenses  #за первый безпроцентный месяц посчитали уже и будем считать за 9 месяцев
+month= 1
+while month <= 9:
+    expenses_procent = expenses * 0.03
+    expenses += expenses_procent
+    money = money + educational_grant - expenses
+    month += 1
+
+money = -round(money,2)
+print(f'Студенту надо попросить {money} рублей')
