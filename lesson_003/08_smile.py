e6c22b05f28d544d2c71f221ef625cd9cb5032c3
Vlad <kill.lilo2@yandex.ru>
2020-05-21 07:04:20 +0300

    08 выполнил

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
-
+import simple_draw as sd
+import random
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
@@ -10,4 +10,16 @@ import simple_draw
 
 # TODO здесь ваш код
 
-simple_draw.pause()
+def smile(x,y,colors):
+    sd.circle(sd.Point(x+11,y+11),6,colors,1)
+    sd.circle(sd.Point(x-9,y+11),6,colors,1)
+    sd.circle(sd.Point(x,y),30,colors,1)
+    sd.line(sd.Point(x-6,y-9),sd.Point(x+7,y-9),colors,1)
+    sd.line(sd.Point(x-6,y-9),sd.Point(x-9,y-6),colors,1)
+    sd.line(sd.Point(x+7,y-9),sd.Point(x+10,y-6),colors,1)
+for i in range(10):
+    x = random.randint(100, 600)
+    y = random.randint(100, 600)
+    smile(x,y,sd.COLOR_WHITE)
+
+sd.pause()
