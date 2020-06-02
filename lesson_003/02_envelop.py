d432ce11810c531201868326d27c924892701188
Vlad <kill.lilo2@yandex.ru>
2020-06-02 04:28:26 +0300

    09 commit

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -35,7 +35,7 @@ else:
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
 
-brick_x, brick_y, brick_z = 8, 10, 9 # TODO хм, этот кирпич должен пролазить
+brick_x, brick_y, brick_z = 10,8, 9
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
@@ -57,12 +57,18 @@ hole_x, hole_y = 8, 9
 can = 0
 
 
-brick = brick_y, brick_x, brick_z
+brick = (brick_x, brick_y, brick_z)
 
 for check in brick:
-    if hole_x >= check and hole_y >= check:
-        can += 1
-if can > 1:
+    if hole_x >= check:
+        can+=1
+for check in brick:
+    if hole_y > check:
+        can+=1
+
+if can>=2:
     print('влезет')
 else:
     print('не влезет')
+
+
