e4ac115847808ff05d58d9a86ef9bee46298085b
Vlad <kill.lilo2@yandex.ru>
2020-06-06 12:12:23 +0300

    02 задание из 5 урока

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,8 +5,9 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from lesson_005 import room_1 as r1,room_2 as r2
 
+print(f"На районе живут {','.join(r1.folks)},{','.join(r2.folks)}")
 
 
 
