5a68da36993c14ae36ad2c289b2604589d269e7b
Vlad <kill.lilo2@yandex.ru>
2020-06-06 12:05:47 +0300

    01 задание из 5 урока

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,6 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from lesson_005 import room_1 as r1,room_2 as r2
+
+print(f'В комнате room_1 живут: {r1.folks}')
+print(f'В комнате room_2 живут: {r2.folks}')
 
 
