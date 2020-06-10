d094006345cdf04a98e9dfa9daf220e5e5cbc92a
Trincha Stanislav <stas.trincha@gmail.com>
2020-06-10 18:27:51 +0300

    5-ый модуль - почти зачет

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -4,7 +4,7 @@
 # Формат: В комнате room_1 живут: ...
 
 import room_1 as r1
-import  room_2 as r2
+import room_2 as r2
 
 print(f'В комнате room_1 живут: {r1.folks}')
 print(f'В комнате room_2 живут: {r2.folks}')
