784ecc86ceaa4949ed20ec78022d8b0e9511155b
Владислав <kill.lilo2@yandex.ru>
2020-08-12 19:02:46 +0300

    Урок 007 Выполнил

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -18,9 +18,131 @@
 # Примеры преобразований:
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
+# Реализовать следующие элементы: Вода, Воздух, Огонь, Земля, Шторм, Пар, Грязь, Молния, Пыль, Лава.
+# Таблица преобразований:
+#   Вода + Воздух = Шторм
+#   Вода + Огонь = Пар
+#   Вода + Земля = Грязь
+#   Воздух + Огонь = Молния
+#   Воздух + Земля = Пыль
+#   Огонь + Земля = Лава
 
-# TODO здесь ваш код
+class Storm:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
+    def __add__(self, other):
+        if isinstance(other,Earth):
+            return Water_Elemental()
+    def __str__(self):
+        return 'Шторм. Состоит из ' + str(self.part1) + ' и ' + str(self.part2)
+class Steam:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
 
+    def __str__(self):
+        return 'Пар. Состоит из ' + str(self.part1) + ' и ' + str(self.part2)
+class Dirt:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
+    def __add__(self, other):
+        if isinstance(other,Fire):
+            return Rock_Elemental()
+
+    def __str__(self):
+        return 'Грязь. Состоит из ' + str(self.part1) + ' и ' + str(self.part2)
+class Light:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
+    def __add__(self, other):
+        if isinstance(other,Earth):
+            return Fire_Elemental()
+    def __str__(self):
+        return 'Молния. Состоит из ' + str(self.part1) + ' и ' + str(self.part2)
+class Dust:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
+
+    def __str__(self):
+        return 'Пыль. Состоит из ' + str(self.part1) + ' и ' + str(self.part2)
+
+
+class Lava:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
+
+    def __str__(self):
+        return 'Лава. Состит из ' + str(self.part1) + ' и ' + str(self.part2)
+class Water:
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm(part1=self, part2=other)
+        elif isinstance(other, Fire):
+            return Steam(part1=Water(),part2=Fire())
+        elif isinstance(other, Earth):
+            return Dirt(part1=Water(),part2=Earth())
+
+
+
+class Fire:
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Lava(part1=self, part2=other)
+
+class Air:
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Dust(part1 = self, part2 = other)
+
+        elif isinstance(other, Fire):
+            return Light(part1=self, part2=other)
+class Earth:
+    def __str__(self):
+        return 'Земля'
+class Rock_Elemental:
+    def __str__(self):
+        return f'Я Каменный элементаль. Состою из: {Water()}, {Earth()}, {Fire()}'
+class Water_Elemental:
+    def __str__(self):
+        return  f'Я Водяной элементаль. Состою из: {Water()}, {Air()}, {Earth()}'
+class Fire_Elemental:
+    def __str__(self):
+        return f'Я Огненный элементаль. Состою из: {Air()}, {Earth()}, {Fire()}'
+
+
+#   Вода + Воздух = Шторм
+#   Вода + Огонь = Пар
+#   Вода + Земля = Грязь
+#   Воздух + Огонь = Молния
+#   Воздух + Земля = Пыль
+#   Огонь + Земля = Лава
+#   Вода + Земля + Огонь = Каменный элементаль:)
+#   Вода + Воздух + Земля = Водяной элементаль
+#   Воздух + Огонь + Земля = Огненный элементаль
+print(Water()+Earth())
+print(Water() + Air())
+print(Water() + Fire())
+print(Water() + Earth())
+print(Air() + Fire())
+print(Air() + Earth())
+print(Fire() + Earth())
+print(Water() + Earth() + Fire())
+print(Water() + Air() + Earth())
+print(Air() + Fire() + Earth())
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
 # Придумать что будет при сложении существующих элементов с новым.
--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -2,6 +2,8 @@
 
 from random import randint
 
+from termcolor import cprint
+
 # Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
 
 # Необходимо создать класс кота. У кота есть аттрибуты - сытость и дом (в котором он живет).
@@ -25,8 +27,137 @@ from random import randint
 
 # Человеку и коту надо вместе прожить 365 дней.
 
-# TODO здесь ваш код
+class Man:
 
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 150
+        self.fullness -= 10
+
+    def clean_up(self):
+        cprint('{} убирался целый день'.format(self.name), color='green')
+        self.fullness -= 20
+        self.house.dirt -= 100
+        if self.house.dirt < 0:
+            self.house.dirt = 0
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой себе и коту'.format(self.name), color='magenta')
+            self.house.money -= 100
+            self.house.food += 50
+            self.house.cat_food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        if self.house.money <= 50:
+            self.work()
+        elif self.fullness <= 20:
+            self.eat()
+        elif self.house.food <= 20 or self.house.cat_food <= 20:
+            self.shopping()
+        elif self.house.dirt > 100:
+            self.clean_up()
+
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.cat_food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 20
+            self.house.cat_food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+    def take_cat_in_to_the_house(self, house):
+        self.house = house
+        cprint('Человек взял кота и назвал его: {}'.format(self.name), color='cyan')
+    def sleep(self):
+        cprint('{} спал целый день'.format(self.name), color='green')
+        self.fullness -= 10
+    def tear_up(self):
+        cprint('{} драл обои'.format(self.name), color='green')
+        self.fullness -= 10
+        self.house.dirt += 5
+    def act(self):
+        if self.fullness <= 10:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        if self.house.dirt >150:
+            cprint('{} сбежал из дома, в доме очень грязно!...'.format(self.name), color='red')
+        dice = randint(1,3)
+        if self.fullness <= 20:
+            self.eat()
+        elif dice == 1:
+            self.tear_up()
+        else:
+            self.sleep()
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.money = 100
+        self.cat_food = 0
+        self.dirt = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, Кошачей еды {}, денег осталось {}, грязь {}'.format(
+            self.food,self.cat_food, self.money, self.dirt)
+
+my_house = House()
+
+me = Man(name='Влад')
+cats = [Cat(name='Кроша'),
+        Cat(name='Масяня'),
+        Cat(name='Жора')]
+# У родителей дома 3 кота, это в честь них я дал им имена)
+
+me.go_to_the_house(house=my_house)
+for cat in cats:
+    cat.take_cat_in_to_the_house(house=my_house)
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    me.act()
+    for cat in cats:
+        cat.act()
+
+    print('--- в конце дня ---')
+    print(me)
+    for cat in cats:
+        print(cat)
+    print(my_house)
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
 # Им всем вместе так же надо прожить 365 дней.
