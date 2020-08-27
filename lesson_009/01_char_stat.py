5a69f034636dacc2528dbc07488b70fc40bcb231
Владислав <kill.lilo2@yandex.ru>
2020-08-27 20:51:55 +0300

    Урок 009 выполнил первый этап

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import zipfile
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
 # Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
@@ -25,7 +25,64 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+file_name = 'python_snippets\\voyna-i-mir.txt'
+
+class CharStat:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.chars = {}
+        self.count = 0
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self._collect_for_line(line=line[:-1])
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self._count_for_line(line=line[:-1])
+
+
+    def _collect_for_line(self, line):
+        for char in line:
+            if char.isalpha():
+                self.chars[char] = 0
+
+    def _count_for_line(self, line):
+        for char in line:
+            if char.isalpha():
+                self.chars[char] += 1
+                self.count += 1
+
+    def prepare(self):
+        self.list_chars = {}
+        self.list_chars = list(self.chars.items())
+        self.list_chars.sort(key=lambda i: i[1], reverse=True)
+
+    def __str__(self):
+        print('+-------------+--------------+')
+        print('|    буква    |    частота   |')
+        print('+-------------+--------------+')
+        for value in self.list_chars:
+            print(f'|      {value[0]}      |    {value[1]:6d}    |')
+        print('+-------------+--------------+')
+        print(f'|    итого    |    {self.count}   |')
+        print('+-------------+--------------+')
+        return ''
+
+files = CharStat(file_name='voyna-i-mir.txt')
+files.collect()
+files.prepare()
+print(files)
+
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
