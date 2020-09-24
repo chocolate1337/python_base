9178c6e90e8fe78c6321510540af8928b70fd9b8
Владислав Бысев <kill.lilo2@yandex.ru>
2020-09-24 15:03:11 +0300

    Задание 012 threads

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -17,6 +17,55 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/01_volatility.py !!!
+import os
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+from threading import Thread
 
-# TODO тут ваш код в многопоточном стиле
+
+class Volatility(Thread):
+
+    def __init__(self, dir_in, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dir_in = os.path.normpath(dir_in)
+        self.prices = defaultdict(float)
+        self.l_prices = []
+        self.zero_volatility = []
+
+    def collect_prices(self):
+        for root, dirs, filenames in os.walk(self.dir_in):
+            for file in filenames:
+                full_file_path = os.path.join(root, file)
+                names = file[7:11]
+                df = pd.read_csv(full_file_path, dtype={'PRICE': np.float32})
+                new_date = df['PRICE'].describe(include='number')
+                half_sum = (new_date.loc['max'] + new_date.loc['min']) / 2
+                volatility = ((new_date.loc['max'] - new_date.loc['min']) / half_sum) * 100
+                if volatility == 0:
+                    self.zero_volatility.append(names)
+                else:
+                    self.prices[names] = volatility
+
+    def run(self):
+        self.collect_prices()
+        self.l_prices = list(self.prices.items())
+        self.l_prices.sort(key=lambda i: i[1])
+        print('Максимальная волатильность:')
+        print(self.l_prices[-3:])
+        print('Минимальная волатильность:')
+        print(self.l_prices[:3])
+        print('Нулевая волатильность:')
+        print(self.zero_volatility)
+
+path = os.path.join(os.path.dirname(__file__), 'trades')
+get_violatity = Volatility(dir_in=path)
+get_violatity.run()
+volatility = [Volatility(dir_in=path), Volatility(dir_in=path)]
+
+def run_in_threads(volatility):
+    for vol in volatility:
+        vol.start()
+    for vol in volatility:
+        vol.join()
+run_in_threads(volatility)
