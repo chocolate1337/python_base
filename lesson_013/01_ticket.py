ff9bda600384b371570f5e4ffaddb29664769018
Владислав Бысев <kill.lilo2@yandex.ru>
2020-10-22 13:45:01 +0300

    Задание 013 выполнил

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+
+
 
 # Заполнить все поля в билете на самолет.
 # Создать функцию, принимающую параметры: ФИО, откуда, куда, дата вылета,
@@ -8,10 +12,22 @@
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
-def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+def make_ticket(fio, from_ , to, data):
+    image = os.path.join('images', 'ticket_template.png')
+    fonts = os.path.join('fonts', 'ofont.ru_Cyntho Next Slab.ttf')
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype(fonts, size=20)
+    w, h = im.size
+    draw.text((45, 120), fio, font=font, fill=ImageColor.colormap['black'])
+    draw.text((45, 190), from_, font=font, fill=ImageColor.colormap['black'])
+    draw.text((45, 260), to, font=font, fill=ImageColor.colormap['black'])
+    draw.text((260, 260), data, font=font, fill=ImageColor.colormap['black'])
+    im.save('probe.png')
+    print(f'Post card saved az probe.png')
 
+if __name__ == '__main__':
+    make_ticket(fio='Бысев В.В',from_='Australia',to='New Zeuland', data='22.10.2020')
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
 # Скрипт должен принимать параметры:
