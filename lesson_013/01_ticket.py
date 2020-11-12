f983fa4f8a6a348f59b7584b2d743e805ba62ca9
Владислав Бысев <kill.lilo2@yandex.ru>
2020-11-12 18:12:47 +0300

    Задание 013 выполнил

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 import argparse
 from datetime import datetime
 
+
 # Заполнить все поля в билете на самолет.
 # Создать функцию, принимающую параметры: ФИО, откуда, куда, дата вылета,
 # и заполняющую ими шаблон билета Skillbox Airline.
@@ -12,24 +13,21 @@ from datetime import datetime
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
-
-parser = argparse.ArgumentParser(description='Заполнение билета')
-parser.add_argument('-fio','--fio',type=str,help='ФИО')
-parser.add_argument('-fr','--fr',type=str,help='Откуда летим')
-parser.add_argument('-to','--to',type=str,help='Куда летим')
-parser.add_argument('-d','--date', type=str, help='Когда летим')
-args = parser.parse_args()
 numbers = '1234567890'
 
-class Validation:
 
-    def __init__(self, date, fio):
+class Photo:
+
+    def __init__(self, to, out, date, fio):
         self.date = date
         self.fio = fio
+        self.to = to
+        self.out = out
 
     def validate(self):
         self.v_data()
         self.v_fio()
+        self.make_ticket()
 
     def v_data(self):
 
@@ -38,32 +36,37 @@ class Validation:
         if valid_date < now:
             parser.error('-d не верная дата')
 
-
     def v_fio(self):
         for number in numbers:
             if number in self.fio:
                 parser.error('-fio маска Фамилия И.О')
-# TODO тут по кодингстайлу сначала идут классы/ф-ии потом код, который с ними работает.
-valid = Validation(date=args.date,fio=args.fio)
-valid.validate()
 
-# TODO почему бы не создать класс с атрибутами полей билета и методами получить билет итд? Также можно создать файл сеттингсов с шрифтом и тд, и оттуда подтягивать данные
-def make_ticket(fio, from_ , to, data):
-    image = os.path.join('images', 'ticket_template.png')
-    fonts = os.path.join('fonts', 'ofont.ru_Cyntho Next Slab.ttf')
-    im = Image.open(image)
-    draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype(fonts, size=20)
-    draw.text((45, 120), fio, font=font, fill=ImageColor.colormap['black'])
-    draw.text((45, 190), from_, font=font, fill=ImageColor.colormap['black'])
-    draw.text((45, 260), to, font=font, fill=ImageColor.colormap['black'])
-    draw.text((260, 260), data, font=font, fill=ImageColor.colormap['black'])
-    im.save('probe.png')
-    print(f'Post card saved az probe.png')
+
+    def make_ticket(self):
+        image = os.path.join('images', 'ticket_template.png')
+        fonts = os.path.join('fonts', 'ofont.ru_Cyntho Next Slab.ttf')
+        im = Image.open(image)
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype(fonts, size=20)
+        draw.text((45, 120), self.fio, font=font, fill=ImageColor.colormap['black'])
+        draw.text((45, 190), self.out, font=font, fill=ImageColor.colormap['black'])
+        draw.text((45, 260), self.to, font=font, fill=ImageColor.colormap['black'])
+        draw.text((260, 260), self.date, font=font, fill=ImageColor.colormap['black'])
+        im.save('probe.png')
+        print(f'Post card saved az probe.png')
+
+
+parser = argparse.ArgumentParser(description='Заполнение билета')
+parser.add_argument('-fio', '--fio', type=str, help='ФИО')
+parser.add_argument('-fr', '--fr', type=str, help='Откуда летим')
+parser.add_argument('-to', '--to', type=str, help='Куда летим')
+parser.add_argument('-d', '--date', type=str, help='Когда летим')
+args = parser.parse_args()
+
 
 if __name__ == '__main__':
-    make_ticket(fio=args.fio, from_=args.fr, to=args.to, data=args.date)
-
+    photo = Photo(date=args.date, fio=args.fio, to=args.to, out=args.fr)
+    photo.validate()
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
